603dfdcfdaf0c4a0e7de1cdd638886c37f70128d
Luke Edwards <luke.edwards05@gmail.com>
2016-08-11 13:39:16 -0700

    add base timeline method

--- lib/wayback_history.ex
+++ lib/wayback_history.ex
@@ -1,2 +1,19 @@
 defmodule WaybackHistory do
+	import HTTPoison, only: [get!: 1]
+
+	def timeline(url) do
+		%{body: data} = wrap(url) |> get!
+		String.split(data, "\n")
+		|> Enum.filter_map(&String.match?(&1, ~r/datetime/),
+			fn row ->
+				Regex.named_captures(~r/<(?<url>[^>]+)>.*datetime="(?<date>[^"]+)"/, row)
+				|> Map.values
+				|> List.to_tuple
+			end)
+	end
+
+	# def urls do
+	# end
+
+	defp wrap(url), do: "http://web.archive.org/web/timemap/link/#{url}"
 end
