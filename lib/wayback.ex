ea5b7c98738557a54ef3783ee0a6dd8e790f26f3
Luke Edwards <luke.edwards05@gmail.com>
2016-08-11 15:50:07 -0700

    fix spacing

--- lib/wayback.ex
+++ lib/wayback.ex
@@ -1,44 +1,44 @@
 defmodule Wayback do
-	@moduledoc """
-	Request moments in history for a URL with the [Wayback Machine](https://archive.org/web/).
-	"""
+  @moduledoc """
+  Request moments in history for a URL with the [Wayback Machine](https://archive.org/web/).
+  """
 
-	import HTTPoison, only: [get!: 1]
+  import HTTPoison, only: [get!: 1]
 
-	@doc """
-	Get the history timeline for a URL.
-	Returns a list of tuples; `{datestring, url}`.
-	"""
-	@spec timeline(String.t) :: [tuple()]
-	def timeline(url) do
-		get!(wrap(url))
-		|> Map.get(:body)
-		|> String.split("\n")
-		|> Enum.filter_map(&String.match?(&1, ~r/datetime/),
-			fn row ->
-				Regex.named_captures(~r/<(?<url>[^>]+)>.*datetime="(?<date>[^"]+)"/, row)
-				|> Map.values
-				|> List.to_tuple
-			end)
-	end
+  @doc """
+  Get the history timeline for a URL.
+  Returns a list of tuples; `{datestring, url}`.
+  """
+  @spec timeline(String.t) :: [tuple()]
+  def timeline(url) do
+    get!(wrap(url))
+    |> Map.get(:body)
+    |> String.split("\n")
+    |> Enum.filter_map(&String.match?(&1, ~r/datetime/),
+      fn row ->
+        Regex.named_captures(~r/<(?<url>[^>]+)>.*datetime="(?<date>[^"]+)"/, row)
+        |> Map.values
+        |> List.to_tuple
+      end)
+  end
 
-	@doc """
-	Return only the url links from a history timeline.
-	"""
-	@spec links(String.t) :: [String.t]
-	def links(url), do: timeline(url) |> Enum.map(fn {_, a} -> a end)
+  @doc """
+  Return only the url links from a history timeline.
+  """
+  @spec links(String.t) :: [String.t]
+  def links(url), do: timeline(url) |> Enum.map(fn {_, a} -> a end)
 
-	@doc """
-	Return the most recent entry from a url's timeline.
-	"""
-	@spec newest(String.t) :: {:datestring, :url}
-	def newest(url), do: timeline(url) |> List.last
+  @doc """
+  Return the most recent entry from a url's timeline.
+  """
+  @spec newest(String.t) :: {:datestring, :url}
+  def newest(url), do: timeline(url) |> List.last
 
-	@doc """
-	Return the oldest entry from a url's timeline.
-	"""
-	@spec oldest(String.t) :: {:datestring, :url}
-	def oldest(url), do: timeline(url) |> List.first
+  @doc """
+  Return the oldest entry from a url's timeline.
+  """
+  @spec oldest(String.t) :: {:datestring, :url}
+  def oldest(url), do: timeline(url) |> List.first
 
-	defp wrap(url), do: "http://web.archive.org/web/timemap/link/#{url}"
+  defp wrap(url), do: "http://web.archive.org/web/timemap/link/#{url}"
 end
--- test/wayback_test.exs
+++ test/wayback_test.exs
@@ -7,29 +7,29 @@ defmodule WaybackTest do
   def is_url(str), do: String.contains?(str, "http")
 
   test "timeline" do
-	res = Wayback.timeline(@url)
-  	assert is_list(res)
-  	assert is_tuple(List.first(res))
+    res = Wayback.timeline(@url)
+    assert is_list(res)
+    assert is_tuple(List.first(res))
   end
 
   test "links" do
-	res = Wayback.links(@url)
-	url = List.first(res)
+    res = Wayback.links(@url)
+    url = List.first(res)
 
-	assert is_list(res)
-	assert is_binary(url)
-	assert is_url(url)
+    assert is_list(res)
+    assert is_binary(url)
+    assert is_url(url)
   end
 
   test "oldest" do
-  	res = {_, url} = Wayback.oldest(@url)
-	assert is_tuple(res)
-	assert is_url(url)
+    res = {_, url} = Wayback.oldest(@url)
+    assert is_tuple(res)
+    assert is_url(url)
   end
 
   test "newest" do
-  	res = {_, url} = Wayback.newest(@url)
-	assert is_tuple(res)
-	assert is_url(url)
+    res = {_, url} = Wayback.newest(@url)
+    assert is_tuple(res)
+    assert is_url(url)
   end
 end
