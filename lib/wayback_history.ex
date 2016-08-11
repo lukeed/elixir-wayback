82f7effcf3008769d0364b26ae962246d2b06342
Luke Edwards <luke.edwards05@gmail.com>
2016-08-11 13:49:09 -0700

    refactor timeline; full pipe sequence

--- lib/wayback_history.ex
+++ lib/wayback_history.ex
@@ -2,8 +2,9 @@ defmodule WaybackHistory do
 	import HTTPoison, only: [get!: 1]
 
 	def timeline(url) do
-		%{body: data} = wrap(url) |> get!
-		String.split(data, "\n")
+		get!(wrap(url))
+		|> Map.get(:body)
+		|> String.split("\n")
 		|> Enum.filter_map(&String.match?(&1, ~r/datetime/),
 			fn row ->
 				Regex.named_captures(~r/<(?<url>[^>]+)>.*datetime="(?<date>[^"]+)"/, row)
